14168034cf0af886eff704a2cd2d4f0992f3da27
Sakthi Ragavan <sakthiragavan359@gmail.com>
2021-05-31 11:37:13 +0530

    program and pin diagram

---
+++ project.ino
@@ -0,0 +1,568 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h> 
+#include <DHT_U.h>
+#include <Adafruit_BMP280.h>
+#include <time.h>
+#include <SPI.h>
+#include <BlynkSimpleEsp32.h>
+
+// Insert your network credentials
+const char* ssid     = "Sakthi";
+const char* password = "12345678";
+#define AUTH "kGjGoKuWpmuBqOI1YavxBp-0WThjUIk0" 
+
+// NTP Server Details
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 16200;
+const int   daylightOffset_sec = 3600;
+
+#define wifiLed   16   //D0
+int wifiFlag = 0;
+float tempe;
+float preas;
+int Humid;
+int light;
+BlynkTimer timer;
+
+// LED's
+#define red 5
+#define blue 32
+#define green 27
+#define yellow 12
+#define ss 2
+#define DHTPIN 0
+DHT_Unified dht(DHTPIN, DHT11);
+
+// OLED Display
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+#define I2Cdisplay_SDA 21
+#define I2Cdisplay_SCL 22
+TwoWire I2Cdisplay = TwoWire(1);
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &I2Cdisplay, -1);
+
+
+// BMP280
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+TwoWire I2CBMP = TwoWire(0);
+Adafruit_BMP280 bmp; // I2C
+
+// Display anime
+#define NUMFLAKES     10 
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+#define XPOS   0 
+#define YPOS   1
+#define DELTAY 2
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,      
+  B00000001, B11000000,      
+  B00000001, B11000000,      
+  B00000011, B11100000,       
+  B11110011, B11100000,     
+  B11111110, B11111000,        
+  B01111110, B11111111,       
+  B00110011, B10011111,        
+  B00011111, B11111100,        
+  B00001101, B01110000,         
+  B00011011, B10100000,        
+  B00111111, B11100000,         
+  B00111111, B11110000,         
+  B01111100, B11110000,         
+  B01110000, B01110000,         
+  B00000000, B00110000 };       
+  const uint8_t *bitmap = logo_bmp;
+  uint8_t w = LOGO_WIDTH;
+  uint8_t h = LOGO_HEIGHT;
+
+// LDR (Light Dependent Resistor)
+#define ldr  33             
+
+int buttonState;              // current reading from the input pin
+int lastButtonState = LOW;    // previous reading from the input pin
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+// Screens
+int displayScreenNum = 0;
+int displayScreenNumMax = 4;
+
+unsigned long lastTimer = 0;
+unsigned long timerDelay = 15000;
+
+unsigned char temperature_icon[] ={
+  0b00000001, 0b11000000, //        ###      
+  0b00000011, 0b11100000, //       #####     
+  0b00000111, 0b00100000, //      ###  #     
+  0b00000111, 0b11100000, //      ######     
+  0b00000111, 0b00100000, //      ###  #     
+  0b00000111, 0b11100000, //      ######     
+  0b00000111, 0b00100000, //      ###  #     
+  0b00000111, 0b11100000, //      ######     
+  0b00000111, 0b00100000, //      ###  #     
+  0b00001111, 0b11110000, //     ########    
+  0b00011111, 0b11111000, //    ##########   
+  0b00011111, 0b11111000, //    ##########   
+  0b00011111, 0b11111000, //    ##########   
+  0b00011111, 0b11111000, //    ##########   
+  0b00001111, 0b11110000, //     ########    
+  0b00000111, 0b11100000, //      ######     
+};
+
+unsigned char humidity_icon[] ={
+  0b00000000, 0b00000000, //                 
+  0b00000001, 0b10000000, //        ##       
+  0b00000011, 0b11000000, //       ####      
+  0b00000111, 0b11100000, //      ######     
+  0b00001111, 0b11110000, //     ########    
+  0b00001111, 0b11110000, //     ########    
+  0b00011111, 0b11111000, //    ##########   
+  0b00011111, 0b11011000, //    ####### ##   
+  0b00111111, 0b10011100, //   #######  ###  
+  0b00111111, 0b10011100, //   #######  ###  
+  0b00111111, 0b00011100, //   ######   ###  
+  0b00011110, 0b00111000, //    ####   ###   
+  0b00011111, 0b11111000, //    ##########   
+  0b00001111, 0b11110000, //     ########    
+  0b00000011, 0b11000000, //       ####      
+  0b00000000, 0b00000000, //                 
+};
+
+unsigned char arrow_down_icon[] ={
+  0b00001111, 0b11110000, //     ########    
+  0b00011111, 0b11111000, //    ##########   
+  0b00011111, 0b11111000, //    ##########   
+  0b00011100, 0b00111000, //    ###    ###   
+  0b00011100, 0b00111000, //    ###    ###   
+  0b00011100, 0b00111000, //    ###    ###   
+  0b01111100, 0b00111110, //  #####    ##### 
+  0b11111100, 0b00111111, // ######    ######
+  0b11111100, 0b00111111, // ######    ######
+  0b01111000, 0b00011110, //  ####      #### 
+  0b00111100, 0b00111100, //   ####    ####  
+  0b00011110, 0b01111000, //    ####  ####   
+  0b00001111, 0b11110000, //     ########    
+  0b00000111, 0b11100000, //      ######     
+  0b00000011, 0b11000000, //       ####      
+  0b00000001, 0b10000000, //        ##       
+};
+
+unsigned char sun_icon[] ={
+  0b00000000, 0b00000000, //                 
+  0b00100000, 0b10000010, //   #     #     # 
+  0b00010000, 0b10000100, //    #    #    #  
+  0b00001000, 0b00001000, //     #       #   
+  0b00000001, 0b11000000, //        ###      
+  0b00000111, 0b11110000, //      #######    
+  0b00000111, 0b11110000, //      #######    
+  0b00001111, 0b11111000, //     #########   
+  0b01101111, 0b11111011, //  ## ######### ##
+  0b00001111, 0b11111000, //     #########   
+  0b00000111, 0b11110000, //      #######    
+  0b00000111, 0b11110000, //      #######    
+  0b00010001, 0b11000100, //    #   ###   #  
+  0b00100000, 0b00000010, //   #           # 
+  0b01000000, 0b10000001, //  #      #      #
+  0b00000000, 0b10000000, //         #       
+};
+
+// Wifi status for blynk
+void checkBlynkStatus() { // called every 2 seconds by SimpleTimer
+
+  bool isconnected = Blynk.connected();
+  if (isconnected == false) {
+    wifiFlag = 1;
+    digitalWrite(wifiLed, LOW); //Turn OFF WiFi LED
+  }
+  if (isconnected == true) {
+    wifiFlag = 0;
+    digitalWrite(wifiLed, HIGH); //Turn ON WiFi LED
+  }
+}
+
+// Create display marker for each screen
+void displayIndicator(int displayNumber) {
+  int xCoordinates[5] = {44, 54, 64, 74, 84};
+  for (int i =0; i<5; i++) {
+    if (i == displayNumber) {
+      display.fillCircle(xCoordinates[i], 60, 2, WHITE);
+    }
+    else {
+      display.drawCircle(xCoordinates[i], 60, 2, WHITE);
+    }
+  }
+}
+
+//SCREEN NUMBER 0: DATE AND TIME
+void displayLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+  //GET DATE
+  //Get full weekday name
+  char weekDay[10];
+  strftime(weekDay, sizeof(weekDay), "%a", &timeinfo);
+  //Get day of month
+  char dayMonth[4];
+  strftime(dayMonth, sizeof(dayMonth), "%d", &timeinfo);
+  //Get abbreviated month name
+  char monthName[5];
+  strftime(monthName, sizeof(monthName), "%b", &timeinfo);
+  //Get year
+  char year[6];
+  strftime(year, sizeof(year), "%Y", &timeinfo);
+
+  //GET TIME
+  //Get hour (12 hour format)
+  /*char hour[4];
+  strftime(hour, sizeof(hour), "%I", &timeinfo);*/
+  
+  //Get hour (24 hour format)
+  char hour[4];
+  strftime(hour, sizeof(hour), "%H", &timeinfo);
+  //Get minute
+  char minute[4];
+  strftime(minute, sizeof(minute), "%M", &timeinfo);
+
+  //Display Date and Time on OLED display
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(3);
+  display.setCursor(19,5);
+  display.print(hour);
+  display.print(":");
+  display.print(minute);
+  display.setTextSize(1);
+  display.setCursor(16,40);
+  display.print(weekDay);
+  display.print(", ");
+  display.print(dayMonth);
+  display.print(" ");
+  display.print(monthName);
+  display.print(" ");
+  display.print(year);
+  displayIndicator(displayScreenNum);
+  display.display();
+  digitalWrite(yellow,LOW);
+}
+
+// SCREEN NUMBER 1: TEMPERATURE
+void displayTemperature(){
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.drawBitmap(15, 5, temperature_icon, 16, 16 ,1);
+  display.setCursor(35, 5);
+  float temperature = tempe;
+  display.print(temperature);
+  display.cp437(true);
+  display.setTextSize(1);
+  display.print(" ");
+  display.write(167);
+  display.print("C");
+  display.setCursor(0, 34);
+  display.setTextSize(1);
+  display.print("Humidity: ");
+  display.print(Humid); 
+  display.print(" %");
+  display.setCursor(0, 44);
+  display.setTextSize(1);
+  display.print("Pressure: ");
+  display.print(preas);
+  display.print(" hpa");
+  displayIndicator(displayScreenNum);
+  display.display();
+  digitalWrite(red,HIGH);
+}
+
+// SCREEN NUMBER 2: HUMIDITY
+void displayHumidity(){
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.drawBitmap(15, 5, humidity_icon, 16, 16 ,1);
+  display.setCursor(35, 5);
+  float humidity = Humid; 
+  display.print(humidity);
+  display.print(" %");
+  display.setCursor(0, 34);
+  display.setTextSize(1);
+  display.print("Temperature: ");
+  display.print(tempe);
+  display.cp437(true);
+  display.print(" ");
+  display.write(167);
+  display.print("C");
+  display.setCursor(0, 44);
+  display.setTextSize(1);
+  display.print("Pressure: ");
+  display.print(preas);
+  display.print(" hpa");
+  displayIndicator(displayScreenNum);
+  display.display();
+  digitalWrite(red,LOW);
+  digitalWrite(blue,HIGH);
+}
+
+// SCREEN NUMBER 3: PRESSURE
+void displayPressure(){
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.drawBitmap(0, 5, arrow_down_icon, 16, 16 ,1);
+  display.setCursor(20, 5);
+  display.print(preas);
+  display.setTextSize(1);
+  display.print(" hpa");
+  display.setCursor(0, 34);
+  display.setTextSize(1);
+  display.print("Altitude: ");
+  display.print(bmp.readAltitude(1013.25));
+  display.cp437(true);
+  display.print(" ");
+  display.write(167);
+  display.print("M");
+  display.setCursor(0, 44);
+  display.setTextSize(1);
+  display.print("Humidity: ");
+  display.print(Humid); 
+  display.print(" hpa");
+  displayIndicator(displayScreenNum);
+  display.display();
+  digitalWrite(blue,LOW);
+  digitalWrite(green,HIGH);
+}
+
+// SCREEN NUMBER 4: LUMINOSITY
+void displayLDR(){
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.drawBitmap(33, 5, sun_icon, 16, 16 ,1);
+  display.setCursor(53, 5);
+  int ldrReading = light;
+  display.print(ldrReading);
+  display.print(" %");
+  display.setTextSize(1);
+  display.setCursor(0, 34);
+  display.print("Temperature: ");
+  display.print(tempe);
+  display.print(" ");
+  display.cp437(true);
+  display.write(167);
+  display.print("C");
+  display.setCursor(0, 44);
+  display.setTextSize(1);
+  display.print("Humidity: ");
+  display.print(Humid); 
+  display.print(" %");
+  display.setCursor(0, 44);
+  displayIndicator(displayScreenNum);
+  display.display(); 
+  digitalWrite(green,LOW);
+  digitalWrite(yellow,HIGH);
+}
+
+// Display the right screen accordingly to the displayScreenNum
+void updateScreen() {
+  if (displayScreenNum == 0){
+    displayLocalTime();
+  }
+  else if (displayScreenNum == 1) {
+    displayTemperature();
+  }
+  else if (displayScreenNum ==2){
+    displayHumidity();
+  }
+  else if (displayScreenNum==3){
+    displayPressure();
+  }
+  else {
+    displayLDR();
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(wifiLed, OUTPUT);
+  pinMode(red,OUTPUT);
+  pinMode(blue,OUTPUT);
+  pinMode(green,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(ss,OUTPUT);
+
+  dht.begin();
+  
+  // Initialize the pushbutton pin as an input
+  
+  I2CBMP.begin(I2C_SDA, I2C_SCL, 100000);
+  I2Cdisplay.begin(I2Cdisplay_SDA, I2Cdisplay_SCL, 100000); 
+
+  // Initialize OLED Display
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  
+  // Initialize BMP280
+  bool status = bmp.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+
+  // Connect to Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  timer.setInterval(2000L, checkBlynkStatus); // check if Blynk server is connected every 2 seconds
+  Blynk.config(AUTH);
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    int8_t f, icons[NUMFLAKES][3];
+    for(f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+    icons[f][YPOS]   = -LOGO_HEIGHT;
+    icons[f][DELTAY] = random(1, 6);
+    Serial.print(F("x: "));
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(F(" y: "));
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(F(" dy: "));
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+
+  boolean b = true;
+  while(WiFi.status() != WL_CONNECTED){ // Loop forever...
+    display.clearDisplay(); // Clear the display buffer
+
+    // Draw each snowflake:
+    for(f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
+    }
+
+    display.display(); // Show the display buffer on the screen
+    delay(50);        // Pause for 1/10 second
+
+    // Then update coordinates of each flake...
+    for(f=0; f< NUMFLAKES; f++) {
+      icons[f][YPOS] += icons[f][DELTAY];
+      // If snowflake is off the bottom of the screen...
+      if (icons[f][YPOS] >= display.height()) {
+        // Reinitialize to a random position, just off the top
+        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+        icons[f][YPOS]   = -LOGO_HEIGHT;
+        icons[f][DELTAY] = random(1, 6);
+      }
+    }
+  }
+}
+
+  Serial.println("WiFi connected.");
+  
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+   /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+}
+
+void loop() {
+  sensors_event_t event;
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Humid = event.relative_humidity;
+  }
+
+  tempe = bmp.readTemperature();
+  display.clearDisplay();
+  // read the state of the switch into a local variable
+  int reading;
+  if (touchRead(T0)>50)
+  {
+    reading = HIGH;
+    if(reading == digitalRead(ss))
+    {
+      reading = LOW;
+    }
+  }else{
+    reading = LOW;
+  }
+
+  preas = bmp.readPressure()/100.0F;
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi Not Connected");
+  }
+  
+  light = map(analogRead(ldr), 0, 4095, 100, 0);
+    light = (light*1.66);
+
+  timer.run();
+  Blynk.run();
+   
+  //Blynk data
+  Blynk.virtualWrite(V1,tempe);
+  Blynk.virtualWrite(V2,Humid);
+  Blynk.virtualWrite(V3,preas);
+  Blynk.virtualWrite(V4,light);
+   Blynk.virtualWrite(V5,bmp.readAltitude(1013.25));
+
+  // Change screen when the pushbutton is pressed
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+        updateScreen();
+        Serial.println(displayScreenNum);
+        if(displayScreenNum < displayScreenNumMax) {
+          displayScreenNum++;
+        }
+        else {
+          displayScreenNum = 0;
+        }
+        lastTimer = millis();
+      }
+    }
+  }
+  lastButtonState = reading;
+  
+  // Change screen every 15 seconds (timerDelay variable)
+  if ((millis() - lastTimer) > timerDelay) {
+    updateScreen();
+    Serial.println(displayScreenNum);
+    if(displayScreenNum < displayScreenNumMax) {
+      displayScreenNum++;
+    }
+    else {
+      displayScreenNum = 0;
+    }
+    lastTimer = millis();
+  }
+}
